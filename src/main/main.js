ee354d003aefe72a463f1c0c2962e8aa39bea66e
Faith Rotich <rotichfaith43@gmail.com>
2025-10-07 14:10:45 +0100

    init d3 + js only project

---
+++ src/main/main.js
@@ -0,0 +1,3 @@
+import flowers from "./viz/flowers";
+
+flowers();
---
+++ src/main/viz/flowers.js
@@ -0,0 +1,25 @@
+import * as d3 from "d3";
+// ...
+// Your code here ...
+// ...
+
+let data;
+
+async function loadData () {
+    data = await d3.dsv(",", "resources/data/natural-disasters-type-pivoted.csv", (d) => {
+        return {
+            code: d.Code,
+            entity: d.Entity,
+            year: new Date(d.Year).getFullYear(),
+            disasterType: d["Natural Disaster"],
+            numDeaths: +d["Number of Deaths"]
+        };
+    });
+}
+
+function main () {
+    console.log("flowers");
+    loadData();
+}
+
+export default main;
